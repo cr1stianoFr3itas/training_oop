d24bb14d469d6f90a148eaeb70ac2e197b419dd1
Cristiano Freitas <66207567+cr1stianoFr3itas@users.noreply.github.com>
2021-06-25 14:58:31 +0200

    Update descontoAvista.java

--- one.digital/src/main/java/atividade/descontoAvista.java
+++ one.digital/src/main/java/atividade/descontoAvista.java
@@ -1,3 +1,12 @@
+/*
+   Dev. Cristiano Freitas
+   email: junior.brown.eda@gmail.com
+   Data de inicio do projeto: 22/06/2021
+   Data de "Final" do projeto: 23/06/2021
+   
+
+*/
+
 package atividade;
 
 public interface descontoAvista{
