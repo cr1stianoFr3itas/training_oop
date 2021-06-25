f114a593a486276bcb47722abc9ee0d04f29553e
Cristiano Freitas <66207567+cr1stianoFr3itas@users.noreply.github.com>
2021-06-25 14:03:01 +0200

    Update lojaGuabiraba.java

--- one.digital/src/main/java/atividade/lojaGuabiraba.java
+++ one.digital/src/main/java/atividade/lojaGuabiraba.java
@@ -1,6 +1,7 @@
 package atividade;
 
 import java.util.*;
+import java.text.*;
 
     public class lojaGuabiraba {
 
@@ -10,30 +11,33 @@ import java.util.*;
 
                 primeiroSemestre primeirosemestre = new primeiroSemestre();
 
-                System.out.println("INICIO PRIMEIRO SEMESTRE...");
+                System.out.println(" INICIO PRIMEIRO SEMESTRE...");
                 System.out.println("\n");
 
-                // VALORES DOS PRODUTOS QUE FORAM VENDIDOS
+                // VALORES DOS PRODUTOS QUE FORAM VENDIDOS DURANTE O SEMESTRE
 
                 primeirosemestre.setValorDoProduto(3.50);
                 primeirosemestre.setValorDoProduto(3.50);
+
+                // TOTAL ADQUIRIDO NO SEMESTRE
 
                 Double totalPimeiroSemestre = primeirosemestre.getValorDoProduto() + primeirosemestre.getValorDoProduto();
 
                 primeirosemestre.setVendasPrimeiroSemestre(totalPimeiroSemestre);
 
 
-                System.out.println("VALOR TOTAL DAS VENDAS DO PRIMEIRO SEMESTRE: R$" + totalPimeiroSemestre);
+                System.out.println(" VALOR TOTAL DAS VENDAS DO PRIMEIRO SEMESTRE: R$" + totalPimeiroSemestre);
                 System.out.println("\n");
 
 
                 // LOJA GUABIRABA PRODUTOS SEGUNDO SEMESTRE
 
                 Scanner scn = new Scanner(System.in);
+                DecimalFormat dcf = new DecimalFormat("#.###");
 
                 segundoSemestre segundosemestre = new segundoSemestre();
 
-                System.out.println("INICIO SEGUNDO SEMESTRE...");
+                System.out.println(" INICIO SEGUNDO SEMESTRE...");
                 System.out.println("\n");
 
 
@@ -42,28 +46,31 @@ import java.util.*;
                 // TIPO DE PAGAMENTO A VISTA
                 double percentualDeDescontoAvista = 0;
                 double produtoComDescontoAvista = 0;
+                double valorDescontoObtidoAvista = 0;
 
                 // TIPO DE PAGAMENTO A PRAZO
                 double produtoComDescontoAprazo = 0;
                 double percentualDeDescontoAprazo = 0;
+                double valorDescontoObtidoAprazo = 0;
 
-                 // VALORES DOS PRODUTOS QUE FORAM VENDIDOS
+                 // VALORES DOS PRODUTOS QUE FORAM VENDIDOS DURANTE O SEMESTRE
 
                  segundosemestre.setValorDoProduto(15.5);
-                 segundosemestre.setValorDoProduto(16.5);
+                 segundosemestre.setValorDoProduto(15.5);
 
+                 // TOTAL ADQUIRIDO NO SEMESTRE
                  Double totalVendidoNoSemestre = segundosemestre.getValorDoProduto() + segundosemestre.getValorDoProduto();
 
-                 // TOTAL VENDIDO
+
                 segundosemestre.setVendasSegundoSemestre(totalVendidoNoSemestre);
-                System.out.println("\nTOTAL DAS VENDAS DO SEGUNDO SEMESTRE: R$" + totalVendidoNoSemestre );
+                System.out.println("\n TOTAL DAS VENDAS DO SEGUNDO SEMESTRE: R$" + totalVendidoNoSemestre );
 
 
             for(int i = 0; i<2 ; i++) {
 
                      // SE O PAGAMENTO A VISTA ESCREVER NUMERO "1" E SE O PAGAMENTO FOR A PRAZO ESCREVER NUMERO "2"
 
-                     System.out.print("\nFORMA DE PAGAMENTO: ");
+                     System.out.print("\n FORMA DE PAGAMENTO: ");
                      String tipoDePagamento = scn.nextLine().toUpperCase();
                      System.out.print("\n");
 
@@ -72,25 +79,30 @@ import java.util.*;
 
 
                          case "V":
+                             // CALCULOS FEITO COM O VALOR TOTAL DA COMPRA
                              percentualDeDescontoAvista = totalVendidoNoSemestre * segundosemestre.getValorDescontoAvista();
-                             System.out.println("PERCENTUAL DE DESCONTO: R$" + percentualDeDescontoAvista);
+                             System.out.println(" VALOR PORCENTAGEM DO DESCONTO: R$" + percentualDeDescontoAvista);
 
-                             System.out.println("VALOR DO DESCONTO %" + segundosemestre.getValorDescontoAvista());
+                             produtoComDescontoAvista = totalVendidoNoSemestre - segundosemestre.getValorDescontoAvista();
+                             System.out.println(" VALOR LIQUIDO: R$" + produtoComDescontoAvista);
 
-                             produtoComDescontoAvista = totalVendidoNoSemestre - percentualDeDescontoAvista;
-                             System.out.println("VALOR LIQUIDO: R$" + produtoComDescontoAvista);
+                             valorDescontoObtidoAvista = produtoComDescontoAvista / totalVendidoNoSemestre;
+                             System.out.println(" VALOR DO DESCONTO OBTIDO: R$" + dcf.format(valorDescontoObtidoAvista));
+
 
                              break;
 
 
                          case "P":
+                             // CALCULOS FEITO COM O VALOR TOTAL DA COMPRA
                              percentualDeDescontoAprazo = totalVendidoNoSemestre * segundosemestre.getValorDescontoAprazo();
-                             System.out.println("PERCENTUAL DE DESCONTO: R$" + percentualDeDescontoAprazo);
+                             System.out.println(" VALOR PORCENTAGEM DO DESCONTO: R$" + dcf.format(percentualDeDescontoAprazo));
 
-                             System.out.println("VALOR DO DESCONTO %" + segundosemestre.getValorDescontoAprazo());
+                             produtoComDescontoAprazo = totalVendidoNoSemestre - segundosemestre.getValorDescontoAprazo();
+                             System.out.println(" VALOR LIQUIDO: R$" + dcf.format(produtoComDescontoAprazo));
 
-                             produtoComDescontoAprazo = totalVendidoNoSemestre - percentualDeDescontoAprazo;
-                             System.out.println("VALOR LIQUIDO: R$" + produtoComDescontoAprazo);
+                             valorDescontoObtidoAprazo = produtoComDescontoAprazo / totalVendidoNoSemestre;
+                             System.out.println(" VALOR DO DESCONTO OBTIDO: R$" + dcf.format(valorDescontoObtidoAprazo));
 
                              break;
 
@@ -104,7 +116,7 @@ import java.util.*;
 
                   // TOTAL PERCENTUAL DESCONTO A VISTA E A PRAZO
                 double valorTotalDesconto = percentualDeDescontoAvista + percentualDeDescontoAprazo;
-                System.out.println("TOTAL DOS DESCONTOS: R$" + valorTotalDesconto);
+                System.out.println("TOTAL DOS DESCONTOS: R$" + dcf.format(valorTotalDesconto));
 
                   // TOTAL LIQUIDO A VISTA E A PRAZO
                 double valorTotalLiquido = produtoComDescontoAvista + produtoComDescontoAprazo;
